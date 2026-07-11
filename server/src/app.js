fed35578e266a35588908e21350aafde19ac6258
Lakshay Sharma <lakshay0328@gmail.com>
2026-07-11 13:13:26 +0530

    initialize server with Express, MongoDB connection, and basic middleware

---
+++ server/src/app.js
@@ -0,0 +1,39 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import helmet from "helmet"
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+const app = express()
+
+app.use(helmet())
+
+app.use(compression())
+
+app.use(morgan("dev"))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(cookieParser())
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running 🚀",
+  });
+});
+
+import notFound from "./middlewares/notFound.js";
+import errorMiddleware from "./middlewares/errorMiddleware.js";
+
+app.use(notFound)
+app.use(errorMiddleware)
+
+export default app
