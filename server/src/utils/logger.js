3cca90f2b7fc0941de33dd59cb09a9b306c213b3
Lakshay Sharma <lakshay0328@gmail.com>
2026-07-12 11:37:20 +0530

    feat(server): add middleware and utility classes for error handling and response management

---
+++ server/src/utils/logger.js
@@ -0,0 +1,9 @@
+const logger = {
+  info: (...args) => console.log(...args),
+
+  error: (...args) => console.error(...args),
+
+  warn: (...args) => console.warn(...args),
+};
+
+export default logger;
